c1c1bfd170d2ae3b28fa5b5653af938a807a2460
Jing Guan <jingg.cxy@foxmail.com>
2016-12-09 17:43:53 +0800

    app: update 2048

--- 2048-master/js/game_manager.js
+++ 2048-master/js/game_manager.js
@@ -69,7 +69,7 @@ GameManager.prototype.addStartTiles = function () {
 GameManager.prototype.addRandomTile = function () {
   if (this.grid.cellsAvailable()) {
     var value = Math.random() < 0.9 ? 2 : 4;
-    var tile = new Tile(this.grid.randomAvailableCell(), value);
+    var tile = new Tile(this.grid.randomAvailableCell(), values[value]);
 
     this.grid.insertTile(tile);
   }
